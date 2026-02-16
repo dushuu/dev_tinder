7bd5b216d1dfa6d556832a30e5bd0f265ee7103a
Dushal <100137660+dushuu@users.noreply.github.com>
2026-02-17 00:34:37 +0530

    send connection api

--- src/routes/requestRouter.js
+++ src/routes/requestRouter.js
@@ -1,13 +1,75 @@
 const express = require("express");
 const profileRouter = express.Router();
 const { userAuth } = require("../middlewares/auth");
+const ConnectionRequest = require("../models/connectionRequest");
 
-profileRouter.post("/sendConnection", userAuth, async (req, res) => {
-  console.log("sending a connection request");
+profileRouter.post("/request/send", userAuth, async (req, res) => {
+  try {
+    const fromUserId = req.user._id;
+    const { toUserId, status } = req.body;
 
-  const user = req.user;
+    const allowedStatus = ["ignore", "interested"];
 
-  res.send(user.firstName + "sent the connection request!");
+    if (!toUserId || !status) {
+      return res.status(400).json({ message: "Missing required fields" });
+    }
+
+    if (!allowedStatus.includes(status)) {
+      return res.status(400).json({ message: "Invalid status type" });
+    }
+
+    // prevent sending to yourself
+    if (fromUserId.toString() === toUserId) {
+      return res.status(400).json({ message: "Cannot send request to yourself" });
+    }
+
+    // check existing request
+    let existingRequest = await ConnectionRequest.findOne({
+      fromUserId,
+      toUserId,
+    });
+
+
+    if (!existingRequest) {
+      const newRequest = new ConnectionRequest({
+        fromUserId,
+        toUserId,
+        status,
+      });
+
+      const data = await newRequest.save();
+
+      return res.json({
+        message:
+          status === "interested"
+            ? "Connection request sent"
+            : "User ignored",
+        data,
+      });
+    }
+
+    
+    if (existingRequest.status === status) {
+      return res.status(400).json({
+        message: `Already marked as ${status}`,
+      });
+    }
+
+
+    existingRequest.status = status;
+    await existingRequest.save();
+
+    return res.json({
+      message:
+        status === "interested"
+          ? "Connection request sent"
+          : "User ignored successfully",
+      data: existingRequest,
+    });
+  } catch (err) {
+    res.status(500).send("Error: " + err.message);
+  }
 });
+
 
 module.exports = profileRouter;
